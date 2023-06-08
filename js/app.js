3d66d86ee9954a36b8f439deeafc1dd9a4623909
acpcarvalhoh <carvalhohuzumak@gmail.com>
2023-06-08 17:30:04 -0300

    verificando jodador vencedor

--- js/app.js
+++ js/app.js
@@ -4,11 +4,50 @@ const App = {
         menuItens: document.querySelector('[data-id="menu-items"'),
         resetBtn: document.querySelector('[data-id="reset-btn"]'),
         newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
-        squares: document.querySelectorAll('[data-id="squares"]')
+        squares: document.querySelectorAll('[data-id="squares"]'),
+        modal: document.querySelector('[data-id="modal"]'),
+        modalText: document.querySelector('[data-id="modal-text"]'),
+        modalBtn: document.querySelector('[data-id="modal-btn"]'),
     },
 
     state: {
         moves: []   
+    },
+
+    getGameStatus(moves){
+
+        const p1Moves = moves.filter(move => move.playerId === 1).map(move => +move.squareId)
+        const p2Moves = moves.filter(move => move.playerId === 2).map(move => +move.squareId)
+
+        // verifique se tem um vecendor ou uma gravata
+        const winningPatterns = [
+            [1, 2, 3],
+            [1, 5, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 5, 7],
+            [3, 6, 9],
+            [4, 5, 6],
+            [7, 8, 9],
+        ];
+
+        let winner = null
+
+        winningPatterns.forEach(pattern => {
+            const p1Wins = pattern.every(v => p1Moves.includes(v))
+            const p2Wins = pattern.every(v => p2Moves.includes(v))
+
+
+            if(p1Wins) winner = 1
+            if(p2Wins) winner = 2
+        })
+
+        
+
+        return{
+            status: moves.length === 9 || winner != null ? 'Jogo completo' : 'Jogo em progresso',
+            winner
+        }
     },
 
     init(){ 
@@ -72,17 +111,17 @@ const App = {
 
                 square.replaceChildren(icon)
 
-                // verifique se tem um vecendor ou uma gravata
-                const winningPatterns = [
-                    [1, 2, 3],
-                    [1, 5, 9],
-                    [1, 4, 7],
-                    [2, 5, 8],
-                    [3, 5, 7],
-                    [3, 6, 9],
-                    [4, 5, 6],
-                    [7, 8, 9],
-                ];
+                // verificar se tem um vencedor 
+                const game = App.getGameStatus(App.state.moves)
+
+                if(game.status === 'Jogo completo'){
+                    if(game.winner){
+                        App.$.modal.classList.remove('hidden')
+                        App.$.modalText.innerHTML = `Jodador ${game.winner} venceu!`
+                    }else{
+                        alert('jogo sem vencedor')
+                    }
+                }
 
             })
 
